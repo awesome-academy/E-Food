0078872e9dd4b905e7f733efc64d195c1e0b9e39
phongvv <vuphong0x@gmail.com>
2021-09-08 09:08:53 +0700

    Refs [TASK][41193] Init MVP architecture

---
+++ app/src/main/java/com/sun/efood/ui/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.sun.efood.ui
+
+import android.os.Handler
+import android.os.Looper
+import com.sun.efood.base.BaseActivity
+import com.sun.efood.databinding.ActivitySplashBinding
+
+private const val TIME_WAITING = 2000L
+
+class SplashActivity : BaseActivity<ActivitySplashBinding>() {
+
+    override fun getViewBinding() = ActivitySplashBinding.inflate(layoutInflater)
+
+    override fun onCreatedView() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(MainActivity.getIntent(this))
+            finish()
+        }, TIME_WAITING)
+    }
+}
